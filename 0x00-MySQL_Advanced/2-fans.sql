4451a841a5ac4b8fb5b4081486439bf6bec23d9a
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-09 17:18:43 +0000

    hendy

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- Script to rank country origins of bands by the total number of fans
 -- Ordered in descending order of total fans
 
-SELECT origin, SUM(fans) AS total_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
