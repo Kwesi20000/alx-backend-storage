aa79a215876c74336f725fe4a9c6270a6d8d0a3d
Kwesi20000 <hendrickskumi@yahoo.com>
2024-05-14 23:18:26 +0000

    hendy

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module that returns the list of schools having a
+specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Function that returns the list of schools having a
+    specific topic
+    """
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
